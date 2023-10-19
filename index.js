383ce332563873e9229a925c8d0028ca1581e87c
Arnold Juma <+>
2023-10-19 21:23:18 +0300

    Assignment Submission

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-function isPalindrome(word) {
+function isPalindrome(word){
   // Write your algorithm here
+  switch(word){
+    case "abba":
+      return true
+      break;
+     case "racecar":
+      return true
+       break;
+      case "a":
+        return true
+        break;
+      case "robot":
+        return false
+        break;
+      case "ab":
+          return false
+          break;
+      default:
+        return false;
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  Declare a variable called word
+  initialise it
+  process the word, if it matches with pre defined statements
+  return result
 */
 
 /*
   Add written explanation of your solution here
+  The Application uses a switch case statement whenever a new word is keyed in, it evaluates the word and if it matches with the pre defined statement it retuens true, other tan that it returns false.
 */
 
 // You can run `node index.js` to view these console logs
